c852f425788d0b9ca2ce4a27d49a40669d14f71b
Mat_Mot <matinmotmaen@gmail.com>
2024-01-29 02:00:03 +0330

    creating custom user model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=25, null=False, blank=False, unique=True)
+    password = models.CharField(max_length=100)
+    repetitions = models.IntegerField(default=10)  # Number of repeat for hashing
+
+    def __str__(self):
+        return f'{self.username}-{self.repetitions}'
